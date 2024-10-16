af1e1c0141cad551161b953d4f8b3bfd8922622d
1 <test@tut.by>
2024-10-16 12:28:31 +0300

    commit changes

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,35 @@
 package com.epam.mjc.nio;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+
+        Map<String, String> dataMap = new HashMap<>();
+        Path path = Paths.get(file.getPath());
+        try (BufferedReader reader = Files.newBufferedReader(path)){
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] keyValue = line.split(": ");
+                dataMap.put(keyValue[0].trim(), keyValue[1].trim());
+            }
+
+        } catch (IOException e) {
+            Logger.getLogger("IOException : "+ Arrays.toString(e.getStackTrace()));
+        }
+
+        return new Profile(dataMap.get("Name"),Integer.parseInt(dataMap.get("Age")),dataMap.get("Email"),Long.parseLong(dataMap.get("Phone")));
     }
 }
+
